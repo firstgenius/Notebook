d9f3a26f9f57bc75c1f7f7843a2b7af384530040
firstgenius <yaroslav.brovchenko@ucu.edu.ua>
2021-03-01 21:31:24 +0200

    Upgraded everything

--- notebook.py
+++ notebook.py
@@ -1,5 +1,6 @@
 import datetime
 
+# Store the next available id for all new notes
 last_id = 0
 
 class Note:
@@ -19,15 +20,18 @@ class Note:
         global last_id
         last_id += 1
         self.id = last_id
-    
+
+
     def match(self, filter):
         '''
         Determine if this note matches the filter
         text. Return True if it matches, False otherwise.
+
         Search is case sensitive and matches both text and
         tags.
         '''
         return filter in self.memo or filter in self.tags
+
 
 class Notebook:
     '''
@@ -35,33 +39,14 @@ class Notebook:
     modified, and searched.
     '''
     def __init__(self):
-        '''
-        Initialize a notebook with an empty list.
-        >>> n = Notebook()
-        >>> n.notes
-        []
-        '''
+        '''Initialize a notebook with an empty list.'''
         self.notes = []
 
+
     def new_note(self, memo, tags=''):
-        '''
-        Create a new note and add it to the list.
-        >>> n = Notebook()
-        >>> n.new_note("hello world")
-        >>> n.new_note("hello again")
-        >>> n.notes[0].id
-        5
-        '''
+        '''Create a new note and add it to the list.'''
         self.notes.append(Note(memo, tags))
 
-    def _find_note(self, note_id):
-        '''
-        Locate the note with the given id.
-        '''
-        for note in self.notes:
-            if note.id == note_id:
-                return note
-        return None
 
     def modify_memo(self, note_id, memo):
         '''
@@ -74,16 +59,17 @@ class Notebook:
             return True
         return False
 
+
     def modify_tags(self, note_id, tags):
         '''
         Find the note with the given id and change its
         tags to the given value.
         '''
-        note = self._find_note(note_id)
-        if note:
-            note.tags = tags
-            return True
-        return False
+        for note in self.notes:
+            if note.id == note_id:
+                note.tags = tags
+                break
+
 
     def search(self, filter):
         '''
@@ -92,3 +78,13 @@ class Notebook:
         '''
         return [note for note in self.notes if
         note.match(filter)]
+
+
+    def _find_note(self, note_id):
+        '''
+        Locate the note with the given id.
+        '''
+        for note in self.notes:
+            if str(note.id) == str(note_id):
+                return note
+        return None
